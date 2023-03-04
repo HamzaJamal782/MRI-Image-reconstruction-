77effd0727f7e8e20caa5ee2f55f771483d93f4b
Your Name <hamzajamal782@gmail.com>
2023-03-04 13:04:06 +0200

    update design

--- main.py
+++ main.py
@@ -27,10 +27,26 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         # connect ui elements to functions
         self.ui.pushButton_Browse.clicked.connect(self.browse)
         self.ui.comboBox_property.currentIndexChanged.connect(self.combobox)
+        self.ui.label_view_img.mousePressEvent = self.getPixel
+    
+    # create a function to get the pixel value of the image
+    def getPixel(self, event):
+        #get image from combobox function and convert it to numpy array to get pixel value
+        if self.ui.comboBox_property.currentIndex() == 0:
+            img = qimage2ndarray.rgb_view(self.image_orignal)
+        elif self.ui.comboBox_property.currentIndex() == 1:
+            img = qimage2ndarray.rgb_view(self.t1(self.image))
+        elif self.ui.comboBox_property.currentIndex() == 2:
+            img = qimage2ndarray.rgb_view(self.t2(self.image))
+        elif self.ui.comboBox_property.currentIndex() == 3:
+            img = qimage2ndarray.rgb_view(self.SD(self.image))
+        else:
+            pass
+        x = event.pos().x()
+        y = event.pos().y()
+        print(img[x,y])
         
-        
-        
-            
+
 
     def browse(self):
         loadImg = QFileDialog.getOpenFileName(self, 'Open file')
@@ -39,7 +55,6 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.ui.label_view_img.setPixmap(QPixmap(self.image_orignal))
 
     # combobox function  for selecting image property
-
     def combobox(self, index):
         if index == 0:
             self.ui.label_view_img.setPixmap(QPixmap(self.image_orignal))
@@ -53,10 +68,11 @@ class ApplicationWindow(QtWidgets.QMainWindow):
             pass
 
     # map range function from brain tissue properties to image pixel values
-    def map_range(self,input_value):
-        output_value = input_value * (255 / 2500)   # 2500 is the max value of the property and 255 is the max pixel value
+    def map_range(self, input_value):
+        # 2500 is the max value of the property and 255 is the max pixel value
+        output_value = input_value * (255 / 300)
         return output_value
-    
+
     def t1(self, in_image):
         # Define the conditionals and corresponding values
         conditions = [
@@ -65,7 +81,8 @@ class ApplicationWindow(QtWidgets.QMainWindow):
             in_image == 90,   # fat
             in_image == 0     # water
         ]
-        values = [self.map_range(500), self.map_range(800), self.map_range(250), self.map_range(3000)]
+        values = [self.map_range(500), self.map_range(
+            800), self.map_range(250), self.map_range(3000)]
 
         # Apply the conditionals and assign values in a single step
         shepp_t1 = np.select(conditions, values, default=255).astype(np.uint8)
@@ -83,7 +100,8 @@ class ApplicationWindow(QtWidgets.QMainWindow):
             in_image == 90,   # fat
             in_image == 0     # water
         ]
-        values = [self.map_range(80), self.map_range(100), self.map_range(55), self.map_range(2000)]
+        values = [self.map_range(80), self.map_range(
+            100), self.map_range(55), self.map_range(2000)]
 
         # Apply the conditionals and assign values in a single step
         shepp_t2 = np.select(conditions, values, default=200).astype(np.uint8)
@@ -102,7 +120,8 @@ class ApplicationWindow(QtWidgets.QMainWindow):
             in_image == 90,   # fat
             in_image == 0     # water
         ]
-        values = [self.map_range(0.1), self.map_range(0.2), self.map_range(0.5), self.map_range(0.7)]
+        values = [self.map_range(0.1), self.map_range(
+            0.2), self.map_range(0.5), self.map_range(0.7)]
 
         # Apply the conditionals and assign values in a single step
         shepp_SD = np.select(conditions, values, default=0.7).astype(np.uint8)
